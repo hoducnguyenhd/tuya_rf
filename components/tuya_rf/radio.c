2ad9841986e0e528b2b88d2ca18b0fd35f56e800
hoducnguyenhd <115439680+hoducnguyenhd@users.noreply.github.com>
2026-03-01 19:51:09 +0700

    Update radio.c

--- components/tuya_rf/radio.c
+++ components/tuya_rf/radio.c
@@ -2,82 +2,11 @@
 #include "cmt2300a.h"
 
 
-
 void Radio_Init(void)
 {
     CMT2300A_Init();
 
-    delay(30);
-
-    CMT2300A_GoStby();
-
     delay(20);
-
-    CMT2300A_ClearInterruptFlags();
-    CMT2300A_ClearRxFifo();
-
-    delay(10);
-
-    CMT2300A_GoRx();
-
-    delay(50);
-
-    CMT2300A_ClearInterruptFlags();
-    CMT2300A_ClearRxFifo();
-}
-
-
-
-/* ========================= */
-/* REQUIRED BY tuya_rf_libretiny */
-/* ========================= */
-
-int StartRx(void)
-{
-
-    CMT2300A_GoStby();
-
-    delay(10);
-
-    CMT2300A_ClearInterruptFlags();
-    CMT2300A_ClearRxFifo();
-
-    delay(10);
-
-    CMT2300A_GoRx();
-
-    delay(40);
-
-    CMT2300A_ClearInterruptFlags();
-    CMT2300A_ClearRxFifo();
-
-    return 1;
-}
-
-
-
-int StartTx(void)
-{
-
-    CMT2300A_GoStby();
-
-    delay(10);
-
-    CMT2300A_ClearInterruptFlags();
-
-    delay(5);
-
-    CMT2300A_GoTx();
-
-    delay(10);
-
-    return 1;
-}
-
-
-
-int RestartRx(void)
-{
 
     CMT2300A_GoSleep();
 
@@ -94,7 +23,70 @@ int RestartRx(void)
 
     CMT2300A_GoRx();
 
-    delay(40);
+    delay(30);
+
+    CMT2300A_ClearInterruptFlags();
+    CMT2300A_ClearRxFifo();
+}
+
+
+
+int StartRx(void)
+{
+    CMT2300A_GoStby();
+
+    delay(5);
+
+    CMT2300A_ClearInterruptFlags();
+    CMT2300A_ClearRxFifo();
+
+    delay(5);
+
+    CMT2300A_GoRx();
+
+    delay(25);
+
+    CMT2300A_ClearInterruptFlags();
+    CMT2300A_ClearRxFifo();
+
+    return 1;
+}
+
+
+
+int StartTx(void)
+{
+    CMT2300A_GoStby();
+
+    delay(5);
+
+    CMT2300A_ClearInterruptFlags();
+
+    delay(5);
+
+    CMT2300A_GoTx();
+
+    delay(5);
+
+    return 1;
+}
+
+
+
+int RestartRx(void)
+{
+    CMT2300A_GoStby();
+
+    delay(5);
+
+    CMT2300A_ClearInterruptFlags();
+    CMT2300A_ClearRxFifo();
+
+    delay(5);
+
+    CMT2300A_GoRx();
+
+    delay(25);
 
     CMT2300A_ClearInterruptFlags();
     CMT2300A_ClearRxFifo();
