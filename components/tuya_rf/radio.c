dab44f2c670dff8f6531ba93d82eb80813dda714
hoducnguyenhd <115439680+hoducnguyenhd@users.noreply.github.com>
2026-03-01 19:24:53 +0700

    Update radio.c

--- components/tuya_rf/radio.c
+++ components/tuya_rf/radio.c
@@ -4,12 +4,11 @@
 void Radio_Init(void)
 {
     CMT2300A_Init();
-    CMT2300A_Config();
-    CMT2300A_SetFrequencyChannel(0);
 
     delay(20);
 
-    CMT2300A_GoStandby();
+    CMT2300A_GoStby();
+
     delay(10);
 
     CMT2300A_GoRx();
@@ -19,9 +18,10 @@ void Radio_Init(void)
     CMT2300A_ClearInterruptFlags();
 }
 
+
 void Radio_StartRx(void)
 {
-    CMT2300A_GoStandby();
+    CMT2300A_GoStby();
 
     delay(5);
 
@@ -36,13 +36,14 @@ void Radio_StartRx(void)
     CMT2300A_ClearInterruptFlags();
 }
 
+
 void Radio_RestartRx(void)
 {
     CMT2300A_GoSleep();
 
     delay(5);
 
-    CMT2300A_GoStandby();
+    CMT2300A_GoStby();
 
     delay(5);
 
